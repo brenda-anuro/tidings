c2f386faf5e0481659e15f6308941a1a9d006296
brenda-anuro <suzannebrendah@gmail.com>
2022-04-06 04:04:07 -0930

    routes, views and functionalities for login and signup done

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-import express from 'express'
-import mysql from 'mysql'
-import bcrypt from 'bcrypt'
-import session from 'express-session'
+const express = require('express')
+const mysql = require('mysql')
+const bcrypt = require('bcrypt')
+const session = ('express-session')
 
 const app = express()
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'tidings'
+})
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -19,6 +26,98 @@ app.get('/about', (req, res)=>{
 })
 
 
+
+
+app.get('/login', (req, res)=>{
+    let user ={
+        email: '',
+        password: '',
+    }
+        res.render('login.ejs', {error:false, user:user})
+})
+app.post('/login',(req,res)=>{
+    let user ={
+        email: req.body.email,
+        password: req.body.password
+    }
+   connection.query(
+       'SELECT * FROM users WHERE email =?', [user.email],
+       (error,results)=> {
+           if(results.length > 0){
+               bcrypt.compare(user.password, results[0].password,(error, isEqual)=>{
+                   if(isEqual){
+                        res.redirect('/')
+                   }else{
+                       let message = 'email/password mismatch.'
+                       res.render('login.ejs', {error:true, message:message, user:user})
+                   }
+               })
+
+           }else{
+               let message = 'Account does not exist. Please create an account'
+               res.render('login.ejs', {error:true, message:message, user:user})
+           }
+       }
+   ) 
+
+
+})
+
+ app.get('/signup', (req,res)=>{
+   let user = {
+       email: '',
+       fullname: '',
+       gender: '',
+       password: '',
+       confirmPassword: '',
+
+   }
+    res.render('signup.ejs', {
+        error: false, user: user
+    })
+})
+app.post('/signup',(req,res)=>{
+    let user = {
+        email: req.body.email,
+        fullname: req.body.fullname,
+        gender: req.body.gender,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+    }
+
+    if (user.password === user.confirmPassword){
+        connection.query(
+            'SELECT email FROM users WHERE email = ?', [user.email],
+            (error, results)=>{
+                if(results.length === 0){
+                    bcrypt.hash(user.password,10, (error,hash)=>{
+                        connection.query(
+                            'INSERT INTO users (email,fullname,gender,password) VALUES (?,?,?,?)',
+                            [user.email, user.fullname, user.gender, hash],
+                            (error,results)=>{
+                                res.redirect('/')
+                            }           
+                        )
+                    })
+                }else {
+                    let message = 'email already exists.'
+                    res.render('sigup.ejs',{error:true, message:message, user:user})
+                }
+            }
+        )
+
+    }else{
+        let message = 'Password and Confirm password does not match'
+        console.log(user)
+        res.render('signup.ejs', {error:true, message:message, user:user})
+    }
+
+
+})
+
+
+                
+                
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
     console.log(`Server up on PORT ${PORT}`)
